f0d427913e95768c35c0a7c8ed8c8948a6800fde
Veera <sveera.2001@gmail.com>
2022-11-08 21:50:56 -0500

    Read all bytes from a server

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,18 @@ fn main() -> std::io::Result<()> {
     let mut stream = TcpStream::connect("example.org:80")?;
     let message = "GET /index.html HTTP/1.0\r\nHost: example.org\r\n\r\n";
     stream.write(message.as_bytes())?;
-    let mut buffer = [0; 128];
-    stream.read(&mut buffer)?;
-    let output = std::str::from_utf8(&buffer).unwrap();
-    println!("{:?}", output);
+    let mut buffer = [0; 1024];
+    loop {
+        if let Ok(size) = stream.read(&mut buffer) {
+            if size == 0 {
+                break;
+            } else {
+                let output = std::str::from_utf8(&buffer[0..size]).unwrap();
+                print!("{}", output);
+            }
+        } else {
+            eprintln!("Error reading from server.");
+        }
+    }
     Ok(())
 }
