de198ea363cb5840a1f7af2c4239ad975647bf96
Veera <sveera.2001@gmail.com>
2022-11-09 15:25:26 -0500

    Move code to get webage into a function

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
 
-fn main() -> std::io::Result<()> {
-    let mut stream = TcpStream::connect("example.org:80")?;
-    let message = "GET /index.html HTTP/1.0\r\nHost: example.org\r\n\r\n";
-    stream.write_all(message.as_bytes());
+fn get_webpage(host: &str, path: &str, port: u16) -> std::io::Result<String> {
+    let host_and_port = format!("{}:{}", host, port);
+    let mut stream = TcpStream::connect(host_and_port)?;
+    let message = format!("GET {} HTTP/1.0\r\nHost: {}\r\n\r\n", path, host);
+    stream.write_all(message.as_bytes())?;
     let mut buffer = [0; 1024];
+    let mut output = String::new();
     loop {
         match stream.read(&mut buffer) {
             Ok(size) if size == 0 => break,
-            Ok(size) => {
-                let output = std::str::from_utf8(&buffer[0..size]).unwrap();
-                print!("{output}");
-            }
+            Ok(size) => output.push_str(std::str::from_utf8(&buffer[0..size]).unwrap()),
             Err(_) => eprintln!("Error reading from server."),
         }
     }
+    Ok(output)
+}
+
+fn main() -> std::io::Result<()> {
+    let data = get_webpage("example.org", "/index.html", 80)?;
+    print!("{}", data);
     Ok(())
 }
