1156cbc0bafca9e116523fdac573a8b30a436fb2
Veera <sveera.2001@gmail.com>
2023-04-30 17:39:19 -0400

    Clean HTTP client

--- src/request.rs
+++ src/request.rs
@@ -5,85 +5,115 @@ use std::net::TcpStream;
 use std::{fmt, fmt::Display};
 
 #[derive(Default, Debug)]
+pub enum StatusCode {
+    Informational,
+    Successful,
+    Redirection,
+    ClientError,
+    ServerError,
+    #[default]
+    Unknown,
+}
+
+#[derive(Default, Debug)]
 pub struct Response {
-    status: String,
+    status: StatusCode,
     header: HashMap<String, String>,
     body: String,
 }
 
 #[derive(Debug)]
 pub enum RequestError {
-    E404,
-    E401,
-    E400,
+    Status(StatusCode),
     Irregular(String),
-    Unknown,
 }
 
 impl std::fmt::Display for RequestError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            RequestError::E404 => write!(f, "404"),
-            RequestError::E401 => write!(f, "401"),
-            RequestError::E400 => write!(f, "400"),
+            RequestError::Status(code) => {
+                write!(f, "Error code: {code:?}")
+            }
             RequestError::Irregular(msg) => {
                 write!(f, "Malformed HTTPS Response: `{msg}`")
             }
-            _ => write!(f, "Unknown Error"),
         }
     }
 }
 
 impl std::error::Error for RequestError {}
 
-impl From<Option<u16>> for RequestError {
-    fn from(number: Option<u16>) -> Self {
-        match number {
-            Some(404) => RequestError::E404,
-            Some(400) => RequestError::E400,
-            Some(401) => RequestError::E401,
-            Some(_) | None => RequestError::Unknown,
+impl From<Option<u16>> for StatusCode {
+    fn from(status: Option<u16>) -> Self {
+        match status {
+            Some(100..=199) => StatusCode::Informational,
+            Some(200..=299) => StatusCode::Successful,
+            Some(300..=399) => StatusCode::Redirection,
+            Some(400..=499) => StatusCode::ClientError,
+            Some(500..=599) => StatusCode::ServerError,
+            Some(_) | None => StatusCode::Unknown,
         }
     }
 }
 
 impl Response {
-    fn parse_status(status: &str) -> Option<u16> {
-        let mut status_split = status.split(" ");
-        status_split.next();
-        status_split.next().and_then(|s| s.parse().ok())
+    fn parse_status(status: Option<&str>) -> StatusCode {
+        if let Some(status) = status {
+            let mut status_split = status.split(" ");
+            status_split.next();
+            let status_number: Option<u16> =
+                status_split.next().and_then(|s| s.parse().ok());
+            StatusCode::from(status_number)
+        } else {
+            StatusCode::Unknown
+        }
     }
-    
+
+    fn parse_header(
+        header: Option<&str>,
+    ) -> Result<HashMap<String, String>, Box<dyn std::error::Error>> {
+        if let Some(header) = header {
+            let mut header = header.split("\r\n");
+            let status = Response::parse_status(header.next());
+            if let StatusCode::Successful = status {
+                let mut map = HashMap::new();
+                for line in header {
+                    let mut key_value = line
+                        .split(':')
+                        .map(|word| word.trim().to_ascii_lowercase());
+                    let key = key_value.next().expect("Key not found.");
+                    let value =
+                        key_value.next().expect("Value not found.");
+                    map.insert(key, value);
+                }
+                Ok(map)
+            } else {
+                Err(Box::new(RequestError::Status(status)))
+            }
+        } else {
+            Err(Box::new(RequestError::Irregular(
+                "Header not found.".into(),
+            )))
+        }
+    }
+
     fn parse_response(
         contents: &str,
     ) -> Result<Response, Box<dyn std::error::Error>> {
-        let mut contents_split = contents.split("\r\n\r\n");
-        let Some(meta) = contents_split.next() else {
-            return Err(Box::new(RequestError::Irregular("Header not found.".into())));
-        };
-        let mut meta_split = meta.split("\r\n");
-        let status = meta_split.next().unwrap();
-        if status.contains("200 OK") {
-            let mut header = HashMap::new();
-            for line in meta_split {
-                let mut key_value = line
-                    .split(':')
-                    .map(|word| word.trim().to_ascii_lowercase());
-                let key = key_value.next().expect("Key not found.");
-                let value = key_value.next().expect("Value not found.");
-                header.insert(key, value);
-            }
-            let Some(body) = contents_split.next() else {
-                return Err(Box::new(RequestError::Irregular("Body not found.".into())));
-            };
+        let mut contents = contents.split("\r\n\r\n");
+        let header = contents.next();
+        let body = contents.last();
+
+        let header = Response::parse_header(header)?;
+
+        if let Some(body) = body {
             Ok(Response {
-                status: status.to_string(),
+                status: StatusCode::Successful,
                 header,
-                body: body.to_string(),
+                body: body.into(),
             })
         } else {
-            let response_code = Response::parse_status(status);
-            Err(Box::new(RequestError::from(response_code)))
+            Err(Box::new(RequestError::Irregular("Body not found.".into())))
         }
     }
 }
